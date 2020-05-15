da061e63800eaa43af49941022879a8f26dc045b
BianSepang <bianconneri46@gmail.com>
2020-05-16 00:06:26 +0800

    misc: edit .repo text

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -125,7 +125,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Click here](https://github.com/keselekpermen69/userbutt) to open UserButt's repo."
+        "[Click here](https://github.com/keselekpermen69/userbutt) to see UserButt's repo.\nor [Click Here](https://github.com/biansepang/userbutt) to see My Forked Repo"
     )
 
 
