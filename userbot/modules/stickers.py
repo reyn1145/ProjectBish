8674558eff3ab03d42284c6240e8005acf0c42c7
reyn1145 <62539100+reyn1145@users.noreply.github.com>
2020-06-22 22:51:34 +0800

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -38,7 +38,7 @@ KANGING_STR = [
 ]
 
 
-@register(outgoing=True, pattern="^.curry")
+@register(outgoing=True, pattern="^.take")
 async def kang(args):
     """ For .kang command, kangs stickers or creates new ones. """
     user = await bot.get_me()
